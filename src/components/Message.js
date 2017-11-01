745d38d97bbc59e483528422db988ad5dad3612b
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-01 17:22:47 -0600

    adding components

---
+++ src/components/Message.js
@@ -0,0 +1,139 @@
+import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap-theme.css'
+// import 'font-awesome/css/font-awesome.css'
+
+class Message extends React.Component {
+
+  //constructor?
+
+  //render
+
+}
+
+
+export default Message
+
+// MESSAGE: UNREAD: UNSTARRED
+// <div class="row message unread">
+//   <div class="col-xs-1">
+//     <div class="row">
+//       <div class="col-xs-2">
+//         <input type="checkbox" />
+//       </div>
+//       <div class="col-xs-2">
+//         <i class="star fa fa-star-o"></i>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="col-xs-11">
+//     <a href="#">
+//       Here is some message text that has a bunch of stuff
+//     </a>
+//   </div>
+// </div>
+
+// MESSAGE: READ: UNSTARRED
+// <div class="row message read">
+//   <div class="col-xs-1">
+//     <div class="row">
+//       <div class="col-xs-2">
+//         <input type="checkbox" />
+//       </div>
+//       <div class="col-xs-2">
+//         <i class="star fa fa-star-o"></i>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="col-xs-11">
+//     <a href="#">
+//       Here is some message text that has a bunch of stuff
+//     </a>
+//   </div>
+// </div>
+
+// MESSAGE SELECTED:
+// <div class="row message read selected">
+//   <div class="col-xs-1">
+//     <div class="row">
+//       <div class="col-xs-2">
+//         <input type="checkbox" checked="checked" />
+//       </div>
+//       <div class="col-xs-2">
+//         <i class="star fa fa-star-o"></i>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="col-xs-11">
+//     <a href="#">
+//       Here is some message text that has a bunch of stuff
+//     </a>
+//   </div>
+// </div>
+
+// MESSAGE STARRED:
+// <div class="row message read">
+//   <div class="col-xs-1">
+//     <div class="row">
+//       <div class="col-xs-2">
+//         <input type="checkbox" />
+//       </div>
+//       <div class="col-xs-2">
+//         <i class="star fa fa-star"></i>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="col-xs-11">
+//     <a href="#">
+//       Here is some message text that has a bunch of stuff
+//     </a>
+//   </div>
+// </div>
+
+// MESSAGE: WITH LABELS
+// <div class="row message read">
+//   <div class="col-xs-1">
+//     <div class="row">
+//       <div class="col-xs-2">
+//         <input type="checkbox" />
+//       </div>
+//       <div class="col-xs-2">
+//         <i class="star fa fa-star"></i>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="col-xs-11">
+//     <span class="label label-warning">dev</span>
+//     <span class="label label-warning">gschool</span>
+//     <a href="#">
+//       Here is some message text that has a bunch of stuff
+//     </a>
+//   </div>
+// </div>
+
+// MESSAGE:EXPANDED
+// <div class="row message read">
+//   <div class="col-xs-1">
+//     <div class="row">
+//       <div class="col-xs-2">
+//         <input type="checkbox" />
+//       </div>
+//       <div class="col-xs-2">
+//         <i class="star fa fa-star"></i>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="col-xs-11">
+//     <span class="label label-warning">dev</span>
+//     <span class="label label-warning">gschool</span>
+//     <a href="#">
+//       Here is some message text that has a bunch of stuff
+//     </a>
+//   </div>
+// </div>
+//
+// <div class="row message-body">
+//   <div class="col-xs-11 col-xs-offset-1">
+//     This is the body of the message.
+//   </div>
+// </div>
---
+++ src/stories/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import Message from '../components/Message'
+import MessageList from '../components/MessageList'
+import Toolbar from '../components/Toolbar'
